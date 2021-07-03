4ec93b400ef2765ed9e6aa0bcd88aa253517c9f9
ChathuraMathula <70540287+ChathuraMathula@users.noreply.github.com>
2021-07-03 12:32:55 +0530

    modified

--- js/script.js
+++ js/script.js
@@ -1,35 +1,36 @@
 $(document).ready(function(){
 
-
-// var SideBarHeight = $(".SideBar").offsetHeight;
-// $(".SideBarContents").css("height", offsetHeight - 120);
-
-
-// (function() {
-// 	var sideHeight = $(".SideBar").css("height");
-// 	$(".SideBarContents").css("height", sideHeight-120);
-// })();
-
-
-	$(document).scroll(function(){
-	var Top = $(document).scrollTop();
-	if (Top > 1) {
-		$(".Header").hide();
-		$(".TopicBar").css("top", 0);
-		$(".SideBarContents").css("top", 45);
-		$(".SideBar").css("padding-top", 45);
-	} else {
-		$(".TopicBar").css("top", 75);
-		$(".Header").show();
-		$(".SideBarContents").css("top", 120);
-		$(".SideBar").css("padding-top", 120);
-	}
+// HEADER HIDE ACTION
+$(document).scroll(function(){
+var Top = $(document).scrollTop();
+if (Top > 1) {
+	$(".Header").hide();
+	$(".TopicBar").css("top", 0);
+	$(".SideBarContents").css("top", 45);
+	$(".SideBar").css("padding-top", 45);
+} else {
+	$(".TopicBar").css("top", 75);
+	$(".Header").show();
+	$(".SideBarContents").css("top", 120);
+	$(".SideBar").css("padding-top", 120);
+}
 });
 
-	$("#content").click(function(){
-		$(".SideBar").toggle();
-	});
+// CONTENT SIDEBAR TOGGLE ACTION
+$("#content").click(function(){
+	$(".SideBar").toggle();
+});
 
+//CONTENT SIDEBARS BLUR EVENT ACTION	
+$("#content").blur(function() {
+	if(window.innerWidth < 992) {
+		$(".SideBar").toggle();
+	}
+});
+	$("#content").click(function () {
+    	$(event.target).focus();
+  	});
+// Action DONE !!
 
 
 
