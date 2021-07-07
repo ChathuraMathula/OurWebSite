c0b9111e18e2f6d9e670622ddb78f171d966676b
ChathuraMathula <70540287+ChathuraMathula@users.noreply.github.com>
2021-07-07 08:53:56 +0530

    modified

--- js/script.js
+++ js/script.js
@@ -17,10 +17,10 @@ $(document).ready(function(){
 	$(document).scroll(function(){
 		if ($(document).scrollTop() > 75) {
 			$("#TopicBar").css("position", "fixed");
-			$(".w3-main").css("padding-top", 45);
+			$("#mainContent").css("padding-top", 45);
 		} else {
 			$("#TopicBar").css("position", "relative");
-			$(".w3-main").css("padding-top", 0);
+			$("#mainContent").css("padding-top", 0);
 		}
 	});
 
@@ -44,7 +44,7 @@ $(document).ready(function(){
 	$("#sideButton").click(function(){
 		$(".w3-sidebar").toggle();
 	});
-	$(".w3-main").click(function(){
+	$("#mainContent").click(function(){
 		$(".w3-sidebar").hide();
 	});
 	$(document).scroll(function(){
@@ -54,8 +54,8 @@ $(document).ready(function(){
 
 	// DARK MODE BUTTON #446f6f
 	$("#dark_mode").click(function(){
-		$(".w3-main").css("background-color", "#619e9e");
-		$(".w3-main").css("color", "black");
+		$("#mainContent").css("background-color", "#619e9e");
+		$("#mainContent").css("color", "black");
 		$(".DARK").css("background-color", "#304f4f").css("color", "white");
 		$("#topicName").css("border-bottom", "1px solid black");
 		$("#main").css("border-right", "1px solid black");
@@ -64,8 +64,8 @@ $(document).ready(function(){
 
 	// Light MODE BUTTON #809fff
 	$("#light_mode").click(function(){
-		$(".w3-main").css("background-color", "#eff5f5");
-		$(".w3-main").css("color", "black");
+		$("#mainContent").css("background-color", "#eff5f5");
+		$("#mainContent").css("color", "black");
 		$(".DARK").css("background-color", "#809fff").css("color", "black");
 		$("#topicName").css("border-bottom", "1px solid #d9d9d9");
 		$("#main").css("border-right", "1px solid #d9d9d9");
